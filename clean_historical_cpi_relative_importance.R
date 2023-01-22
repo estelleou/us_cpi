eb59128012f3fe28abc137a909798fdea0b2cc9f
Estelle Ou <estelle0518@gmail.com>
2023-01-21 22:46:00 -0500

    initial commit of scripts that create a historical time-series of relative cpi weights and calculates a time-series of core services less shelter inflation overtime

---
+++ clean_historical_cpi_relative_importance.R
@@ -0,0 +1,132 @@
+#script cleans the relative importance of CPI items throughout history and combines
+#it into one dataset 
+library(tidyverse)
+library(lubridate)
+
+#latest data
+#manually download the relative importance data from the BLS's website
+#download Table 2 from https://www.bls.gov/web/cpi.supp.toc.htm 
+raw_data <- 
+  readxl::read_xlsx("latest_cpi_data.xlsx", skip = 2)
+
+cleaned_cpi_relative_importance_and_indent_data  <-
+  raw_data %>% 
+  select(`...1`, `...2`, `...3`) %>% 
+  rename(indent_level = `...1`,
+         items= `...2`,
+         weight_c = `...3`) %>% 
+  mutate(weight_c = as.double(weight_c)) %>% 
+  #generating year so that dataset can be merged with other years
+  mutate(date = as.Date("2022", "%Y"),
+         year = year(date)) %>% 
+  mutate(items = gsub("\\(\\d+\\)", "", items),
+         items = gsub("\\d+", "", items),
+         items = str_to_lower(items)) %>% 
+  select(-date) %>% 
+  slice(-(2:3)) %>% 
+  slice(-(1:1))
+  
+#pull in all others through text files downloaded from the BLS's website
+#2000-2009
+cleaned_cpi_relative_importance_2000_to_2009 <- list()
+for (i in seq(2000,2009, by = 1) ){
+
+  file_name <-  paste0("cpi_relative_importance_2000-2009/", i,".txt")
+  
+  raw_data <-
+  read_csv(file_name) %>% 
+  slice(-(1:6)) 
+  
+cleaned_file <-
+  raw_data %>% 
+  separate(names(raw_data), into = c("items", "weight_c", "weight_w"), sep = "    ") %>%
+  select(-weight_w) %>% 
+  mutate(weight_c = as.double(weight_c)) %>% 
+  mutate(items = gsub("\\.+$", "", items)) %>% 
+  mutate(items = gsub("\\(\\d+\\)", "", items),
+         items = gsub("\\d+", "", items)) %>% 
+  mutate(items = str_trim(items)) %>% 
+  #remove the first row that shows "All items with a weight of 100" 
+  #because don't need it and formatting is weird
+  filter(!is.na(weight_c)) %>% 
+  #slapping on the date for the dataset for merging later on
+  mutate(date = as.Date(as.character(i), "%Y"),
+         year = year(date)) %>% 
+  select(-date)
+
+  cleaned_cpi_relative_importance_2000_to_2009 <- bind_rows(cleaned_cpi_relative_importance_2000_to_2009, cleaned_file)
+
+}
+
+
+#2010-2019
+cleaned_cpi_relative_importance_2010_to_2019 <- list()
+for (i in seq(2010,2019, by = 1) ){
+  
+  file_name <-  paste0("cpi_relative_importance_2010-2019/", i,".txt")
+  
+  raw_data <-
+    read_csv(file_name) %>% 
+    slice(-(1:6)) 
+  
+  cleaned_file <-
+    raw_data %>% 
+    separate(names(raw_data), into = c("items", "weight_c", "weight_w"), sep = "    ") %>%
+    select(-weight_w) %>% 
+    mutate(weight_c = as.double(weight_c)) %>% 
+    mutate(items = gsub("\\.+$", "", items)) %>% 
+    mutate(items = gsub("\\(\\d+\\)", "", items),
+           items = gsub("\\d+", "", items)) %>% 
+    mutate(items = str_trim(items)) %>% 
+    #remove the first row that shows "All items with a weight of 100" 
+    #because don't need it and formatting is weird
+    filter(!is.na(weight_c)) %>% 
+    #slapping on the date for the dataset for merging later on
+    mutate(date = as.Date(as.character(i), "%Y"),
+           year = year(date)) %>% 
+    select(-date)
+  
+  cleaned_cpi_relative_importance_2010_to_2019 <- bind_rows(cleaned_cpi_relative_importance_2010_to_2019, cleaned_file)
+  
+}
+
+#cleaning the latest two years (2020-2021)
+cleaned_cpi_relative_importance_2020 <- 
+  readxl::read_xlsx("cpi_relative_importance_2020.xlsx", skip = 8) %>% 
+  rename(items = `All items`,
+       weight_c = `100...3`) %>% 
+  select(items, weight_c) %>% 
+  filter(!is.na(items)) %>% 
+  #slapping on the date for the dataset for merging later on
+  mutate(date = as.Date("2020", "%Y"),
+       year = year(date)) %>% 
+  select(-date)
+
+cleaned_cpi_relative_importance_2021 <- 
+  readxl::read_xlsx("cpi_relative_importance_2021.xlsx", skip = 8) %>% 
+  rename(items = `All items`,
+         weight_c = `100...3`) %>% 
+  select(items, weight_c) %>% 
+  filter(!is.na(items)) %>% 
+  #slapping on the date for the dataset for merging later on
+  mutate(date = as.Date("2021", "%Y"),
+         year = year(date)) %>% 
+  select(-date)
+  
+#combine all the cleaned datasets
+cleaned_cpi_relative_importance_historical_data <-
+  cleaned_cpi_relative_importance_and_indent_data %>% 
+  full_join(cleaned_cpi_relative_importance_2021) %>% 
+  full_join(cleaned_cpi_relative_importance_2020) %>% 
+  full_join(cleaned_cpi_relative_importance_2010_to_2019) %>% 
+  full_join(cleaned_cpi_relative_importance_2000_to_2009) %>% 
+  arrange(desc(items)) %>% 
+  fill(indent_level, .direction = c("down")) %>% 
+  # last bit of cleaning to make merging easier 
+  mutate(items = str_to_lower(items)) %>% 
+  mutate(items = ifelse(items == "recreation", "recreation services", items),
+         items = ifelse(items == "education and communication", "education and communication services", items))
+
+  
+
+  
---
+++ us_core_services_less_shelter_inflation.R
@@ -0,0 +1,127 @@
+#script calculates core Services less shelter CPI 
+library(tidyverse)
+library(lubridate)
+library(scales)
+
+#pull relative importance data and cpi time series ---------------------------
+
+source("clean_historical_cpi_relative_importance.R")
+
+#cpi time series 
+raw_time_series<-
+  #all items
+  read_delim('https://download.bls.gov/pub/time.series/cu/cu.data.1.allitems', col_type = 'c') %>% 
+  #"us food and beverages",
+  full_join(read_delim('https://download.bls.gov/pub/time.series/cu/cu.data.11.USFoodBeverage',  col_type = 'c')) %>%
+  #us commodities services special aggregate,
+  full_join( read_delim('https://download.bls.gov/pub/time.series/cu/cu.data.20.USCommoditiesServicesSpecial',  col_type = 'c')) %>%
+  #"us transportation",
+  full_join( read_delim('https://download.bls.gov/pub/time.series/cu/cu.data.14.USTransportation',   col_type = 'c')) %>%
+  #"us housing,
+  full_join( read_delim('https://download.bls.gov/pub/time.series/cu/cu.data.12.USHousing',  col_type = 'c') )%>%
+  #more items that were dropped",
+  full_join(  read_delim('https://download.bls.gov/pub/time.series/cu/cu.data.0.Current',  col_type = 'c')) %>%
+  #"us medical",
+  full_join( read_delim('https://download.bls.gov/pub/time.series/cu/cu.data.15.USMedical',  col_type = 'c'))
+
+#sector category codes
+services_code_mapping <- 
+  tribble(~id, ~items,
+  "CUSR0000SASLE",	"services less energy services",
+  "CUSR0000SAH1","shelter",
+  "CUSR0000SEHC", "owners' equivalent rent of residences",
+  "CUSR0000SEHC01","rent of primary residence",
+  "CUSR0000SEHB", "lodging away from home",
+  "CUSR0000SEHG","water and sewer and trash collection services",
+  "CUUR0000SEHP","household operations",
+  "CUSR0000SAS4","transportation services",
+  "CUSR0000SETG01","airline fares",
+  "CUSR0000SAM2", "medical care services",
+  "CUSR0000SARS",  "recreation services",
+  "CUSR0000SAES","education and communication services",
+  "CUUR0000SAGS", "other personal services")
+
+category_list <- 
+  unique(services_code_mapping$id)
+
+cleaned_core_services_cpi_data<- 
+  #clean data
+  raw_time_series%>% 
+  #reformat and clean data
+  rename(id = `series_id        `, 
+         year = `year`,
+         month = `period`, 
+         value = `       value`) %>% 
+  select(id, year, month, value) %>% 
+  mutate(id = str_trim(id),
+         value = str_trim(value), 
+         value = as.double(value), 
+         month = str_sub(month, 2, -1), 
+         date = str_c(year, "-", month), 
+         date = ym(date)) %>% 
+  select(date, id, value) %>%
+  filter(id %in% category_list) %>% 
+  #merging mapping
+  left_join(services_code_mapping) %>% 
+  # select(date, value, series_title) %>%
+  #calculating change per month
+  group_by(id) %>% 
+  mutate(monthly_chg = round(((value - lag(value))/lag(value))*100, 1)) %>% 
+  ungroup()
+
+#merging the two datasets together
+core_services_cpi_and_weights_full_dataset <- 
+  cleaned_core_services_cpi_data %>% 
+  #creating year variables so that it can be merged with the weights dataset
+  mutate(year = year(date)) %>% 
+  #merging relative weights data from clean_historical_cpi_relative_importance.R
+  full_join(cleaned_cpi_relative_importance_historical_data) %>% 
+  #don't need these two variables anymore for merging with the weights dataset
+  select(-indent_level)
+
+
+#creating time-series of core services inflation less shelter data--------------
+core_services_less_shelter_categories <- 
+  c("education and communication services", "water and sewer and trash collection services",
+    "recreation services", "transportation services")
+
+core_services_less_shelter_cpi_data <- 
+ core_services_cpi_and_weights_full_dataset %>% 
+  filter(date > as.Date("2010-01-01")) %>% 
+  filter(items %in% core_services_less_shelter_categories) %>% 
+  # calculate weights by year
+  group_by(date) %>% 
+  mutate(denominator = sum(weight_c),
+         core_services_weights = weight_c/denominator,
+         adjusted_contribution = monthly_chg*core_services_weights) %>% 
+  summarize(core_services_less_shelter_inflation = sum(adjusted_contribution)) %>% 
+   ungroup()
+
+#plotting core services less shelter inflation through time ------------------
+  core_services_less_shelter_cpi_data %>% 
+  ggplot() +
+  geom_line(aes(x = date, y = core_services_less_shelter_inflation)) +
+  # geom_bar(aes(x = date, y = core_services_less_shelter_inflation), stat = "identity") +
+  geom_hline(yintercept = 0)+
+  labs(x = " ", y = " ", 
+       title = "U.S. Core Services Less Shelter Inflation (2010-Present)", 
+       subtitle = "% m/m",
+       caption = "Series includes: Education and Communication Services, Water and Sewer and Trash Collection Services, Recreation Services, \nTransportation Services or historical equivalents") + 
+  scale_x_date(lim = c(as.Date("2010-01-01"), as.Date(today())),
+               date_label = "%Y",
+               date_breaks = "1 year") +
+  #my own personal theme setup
+  estelle_theme() +
+  theme(plot.caption= element_text(hjust = 0,
+                                   margin = margin(-10,0,0,0)))
+
+ggsave("us_core_services_less_shelter_inflation.png")
+
+#following the sanity check, realized the weights change every month, not just
+#year so need to go back and adjust that, but we're close
+
+  
+
+
+
+
